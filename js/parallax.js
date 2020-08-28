9a7daa18b6d2f4f71a18499b9bcbd65f8eefa193
Milovan <marjanovic90@gmail.com>
2020-08-29 01:15:03 +0200

    add vision, mission... section

--- js/parallax.js
+++ js/parallax.js
@@ -3,8 +3,8 @@ window.addEventListener("scroll", () => {
   const diamondRight = document.getElementById("heroDiamondRight");
 
   var scrolled = window.pageYOffset;
-  var rateLeft = scrolled * -0.5;
-  var rateRight = scrolled * 0.4;
+  var rateLeft = scrolled * 0.3;
+  var rateRight = scrolled * -0.4;
 
   diamondLeft.style.transform = `translate3d(0px, ${rateLeft}px, 0px)`;
   diamondRight.style.transform = `translate3d(0px, ${rateRight}px, 0px)`;
